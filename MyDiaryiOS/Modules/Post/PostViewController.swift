3372c8181af52cb396d07f4a54dff9b10ec7003b
musashi.sakamoto <Musashi.Sakamoto@cognizant.com>
2019-06-12 10:24:17 +0900

    button配置

--- MyDiaryiOS/Modules/Post/PostViewController.swift
+++ MyDiaryiOS/Modules/Post/PostViewController.swift
@@ -12,12 +12,25 @@ import UIKit
 class PostViewController: UIViewController {
     var presenter: PostPresenterInterface!
 
+    @IBOutlet var postButton: RaisedButton!
     @IBOutlet var titleTextField: TextField!
     @IBOutlet var descriptionTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self._setupView()
+    }
+
+    private func _setupView() {
+        let button = FABButton(image: Icon.cm.photoLibrary, tintColor: .white)
+        button.pulseColor = .white
+        button.backgroundColor = .purple
+        view.layout(button).width(48).height(48).above(self.postButton, 8).trailing(self.postButton)
+
+        self.postButton.pulseColor = .white
+        self.postButton.titleColor = .white
+        self.postButton.backgroundColor = .purple
     }
 
     @IBAction func cancelButtonHandler(_ sender: Button) {
